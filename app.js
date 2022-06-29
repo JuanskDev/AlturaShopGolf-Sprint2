72b0ed900cae9fe1200da782ce90bd9d8d0760fc
acontrer31 <agustincontrerasvidal@gmail.com>
2022-06-29 20:49:18 -0300

    Actualizacion, Header, Registro y Agregue Script e imagenes

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 app.listen(3030, () => {
   console.log("Servidor Corriendo en Puerto 3030");
 });
 
-
-
-// RUTAS  - - --  -- - - - - - - -- -- - - - - 
+// RUTAS  - - --  -- - - - - - - -- -- - - - -
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/home.html"));
@@ -22,14 +19,17 @@ app.get("/checkout", (req, res) => {
   res.sendFile(path.resolve("./Views/Carrito-de-compras.html"));
 });
 
-app.get("/ingreso"), (req,res) => {
-  res.sendFile(path.resolve(__dirname, "./views/InicioSesion.html"))
-};
+app.get("/ingreso"),
+  (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/InicioSesion.html"));
+  };
 
-app.get("/producto"), (req,res) => {
-  res.sendFile(path.resolve(__dirname, "./views/DetalleProducto.html"))
-};
+app.get("/producto"),
+  (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/DetalleProducto.html"));
+  };
 
-app.get("/registro"), (req,res) => {
-  res.sendFile(path.resolve(__dirname, "./views/Registro.html"))
-}
+app.get("/registro"),
+  (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/Registro.html"));
+  };
